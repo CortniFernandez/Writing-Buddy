7d7663240d2aa904495f630f638ee98b2ae648c6
Cortni <cortni.fernandez@gmail.com>
2021-03-08 16:02:19 -0500

    Got today's word count input to update and display

--- src/todays-target/targetApp.js
+++ src/todays-target/targetApp.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './targetApp.css';
 
-const TargetApp = () => {
+function TargetApp() {
+  const [todayGoal, setTodayGoal] = useState(0);
+
+  function handleGoalChange() {
+    setTodayGoal(document.getElementById("goal").value);
+  }
+
+
   return (
     <div className="target-container">
-      <h1>Today's Word Count Goal</h1>
+      <h1 className="target-title">Today's Word Count Goal</h1>
+      <p>Goal today:</p>
+      <input type="number" id="goal" defaultValue="0" onChange={handleGoalChange}></input><button onClick={handleGoalChange}>Let's go!</button>
+      <h5>My goal: {todayGoal}</h5>
     </div>
   )
 }
+
 
 export default TargetApp;
